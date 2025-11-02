4df316eb8a9d2fdeee092205145aaf5bd02227ad
scriptersupra73 <supra.fevon@gmail.com>
2025-11-01 21:34:54 -0400

    Update welcome message image URL

--- ssbot.js
+++ ssbot.js
@@ -30,7 +30,7 @@ client.on('guildMemberAdd', async member => {
       .setDescription('You are member #' + memberCount + ' of Smiley Services.\n⭐⭐⭐⭐⭐ Shop Safe | Develop Quick\n\nUse /ticket to get started.')
       .setColor(0x5865F2)
       .setThumbnail(member.displayAvatarURL())
-      .setImage('https://i.imgur.com/aHOjIoB_d.webp?maxwidth=760&fidelity=grand')
+      .setImage('https://imgur.com/a/Z2GblpN')
       .setFooter({ text: 'Smiley Services Bot', iconURL: client.user.displayAvatarURL() });
     
     await welcomeChannel.send({ content: 'Welcome <@' + member.id + '>!', embeds: [embed] });
